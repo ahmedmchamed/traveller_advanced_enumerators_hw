6d21e2a8f6efe3f27ea37324c8524b890db10303
Ahmed Hamed <tunashark@gmail.com>
2020-06-17 17:16:52 +0100

    traveller mvp

--- hw_enumeration_traveller_start/models/traveller.js
+++ hw_enumeration_traveller_start/models/traveller.js
@@ -3,27 +3,31 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map(journey => journey.startLocation)
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map(journey => journey.endLocation)
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter(journey => journey.transport === transport)
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter(journey => journey.distance > minDistance)
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((totalDistance, journey) => {
+    return totalDistance + journey.distance;
+  }, 0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  return this.journeys.filter((journey, index, journeys) => {
+    return journeys.indexOf(journey) === index
+  })
 };
 
 
