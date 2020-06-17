4cff660f072309d15838b31cb0bf9df30d494225
Ahmed Hamed <tunashark@gmail.com>
2020-06-17 20:00:26 +0100

    pangram working

--- hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
+++ hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,12 @@
 const PangramFinder = function (phrase) {
-  this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
+  this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split(''); //this is an array
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  return this.alphabet.every((letter) => {
+    return this.phrase.toLowerCase().split('').includes(letter);
+  })
 }
 
 module.exports = PangramFinder;
