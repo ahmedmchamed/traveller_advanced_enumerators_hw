6ed862ef75e136fb589ee2f691be197c56460596
Ahmed Hamed <tunashark@gmail.com>
2020-06-17 23:25:38 +0100

    advanced enumeration - slight refactor of anagram

--- hw_advanced_enumeration_start/anagram_finder/anagram_finder.js
+++ hw_advanced_enumeration_start/anagram_finder/anagram_finder.js
@@ -2,13 +2,6 @@ const AnagramFinder = function (word) {
     this.word = word;
 }
 
-//redundant
-// AnagramFinder.prototype.checkIfSameLength = function (otherWords) {
-//     return otherWords.every((word) => {
-//         return this.word.length === word.length 
-//     })
-// }
-
 AnagramFinder.prototype.checkIfIdenticalWord = function (otherWords) {
     return otherWords.every((word) => {
         return this.word === word
@@ -16,11 +9,7 @@ AnagramFinder.prototype.checkIfIdenticalWord = function (otherWords) {
 } 
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-    //redundant
-    // if (!this.checkIfSameLength(otherWords)) {
-    //     return [];
-    // }
-
+    
     if (this.checkIfIdenticalWord(otherWords)) {
         return otherWords.filter((anagram) => {
             return false;
