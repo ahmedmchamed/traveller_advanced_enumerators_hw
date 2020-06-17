ca7a222a743f74180090203c6cc4c7bcbdba2259
Ahmed Hamed <tunashark@gmail.com>
2020-06-17 22:34:21 +0100

    advanced enumeration complete

--- hw_advanced_enumeration_start/anagram_finder/anagram_finder.js
+++ hw_advanced_enumeration_start/anagram_finder/anagram_finder.js
@@ -1,9 +1,33 @@
 const AnagramFinder = function (word) {
-
+    this.word = word;
 }
 
-AnagramFinder.prototype.findAnagrams = function (otherWords) {
+//redundant
+// AnagramFinder.prototype.checkIfSameLength = function (otherWords) {
+//     return otherWords.every((word) => {
+//         return this.word.length === word.length 
+//     })
+// }
 
+AnagramFinder.prototype.checkIfIdenticalWord = function (otherWords) {
+    return otherWords.every((word) => {
+        return this.word === word
+    })
+} 
+
+AnagramFinder.prototype.findAnagrams = function (otherWords) {
+    //redundant
+    // if (!this.checkIfSameLength(otherWords)) {
+    //     return [];
+    // }
+
+    if (this.checkIfIdenticalWord(otherWords)) {
+        return [];
+    }
+
+    return otherWords.filter((anagram) => {
+        return this.word.toLowerCase().split('').sort().join() === anagram.toLowerCase().split('').sort().join()
+    })
 }
 
 module.exports = AnagramFinder;
