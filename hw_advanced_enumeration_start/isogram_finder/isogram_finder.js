b945b56eba84cfe62448e1fc878bf076c19aeb4d
Ahmed Hamed <tunashark@gmail.com>
2020-06-17 20:16:20 +0100

    isogram working

--- hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,11 @@
 const IsogramFinder = function (word) {
-
+    this.word = word;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    return this.word.toLowerCase().split('').every((letter, index, array) => {
+        return array.indexOf(letter) === index;
+    })
 }
 
 module.exports = IsogramFinder;
